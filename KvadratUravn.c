f9f147a58498e4cc87f6091c6034a05b935da758
lev Bagryansky <32939651+levBagryansky@users.noreply.github.com>
2020-09-01 21:07:56 +0300

    Add files via upload

--- KvadratUravn.c
+++ KvadratUravn.c
@@ -3,15 +3,17 @@
 
 const int infinity = -1; // if there are infinity roots
 
-int solve(double a, double b, double c, double* x1, double* x2);
+int solveSQW(double a, double b, double c, double* x1, double* x2);
+int solveLN(double b, double c, double* x1, double* x2); // bx + c = 0
 double findDiscr(double a, double b, double c);
+int choseFunction(double a, double b, double c, double* x1, double* x2);
 
 int main(){
     int nRoots;
     double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
     printf("Enter coefficients a, b and c: \n");
     scanf ("%lg %lg %lg", &a, &b, &c);
-    nRoots = solve(a, b, c, &x1, &x2);
+    nRoots = choseFunction(a, b, c, &x1, &x2);
     switch(nRoots){
         case 0:  printf("There is not any roots \n");
                 break;
@@ -28,28 +30,11 @@ int main(){
 }
 
 
-int solve(double a, double b, double c, double* x1, double* x2)
+int solveSQW(double a, double b, double c, double* x1, double* x2)
 {
     double d = findDiscr(a, b, c);
 
-    if(a == 0 and b == 0 and c == 0) // infinite
-    {
-        return infinity;
-    }
-
-    else if(a == 0 and b == 0)     // for example 0x*x + 0x + 5 = 0
-    {
-        return 0;
-    }
-
-    else if(a == 0)                          // 1 root because a == 0
-    {
-        *x1 = -c/b;
-        *x2 = *x1;
-    return 1;
-    }
-
-    else if(d < 0)                      // 0 roots
+    if(d < 0)                      // 0 roots
     {
         return 0;
     }
@@ -71,8 +56,43 @@ int solve(double a, double b, double c, double* x1, double* x2)
 }
 
 
+int solveLN(double b, double c, double* x1, double* x2)
+{
+    if(b == 0 and c == 0)             // infinite 0x + 0 = 0
+    {
+        return infinity;
+    }
+
+    else if(b == 0)                   // for example 0x + 5 = 0
+    {
+        return 0;
+    }
+
+    else                              // bx + c = 0
+    {
+        *x1 = -c/b;
+        *x2 = *x1;
+    return 1;
+    }
+}
+
+
 double findDiscr(double a, double b, double c)
 {
     double discriminant = b*b - 4*a*c;
     return discriminant;
+}
+
+int choseFunction(double a, double b, double c, double* x1, double* x2)
+{
+    if(a == 0){
+        int nRoots = solveLN(b, c, x1, x2);
+        return nRoots;
+    }
+
+    else
+    {
+        int nRoots = solveSQW(a, b, c, x1, x2);
+        return nRoots;
+    }
 }
