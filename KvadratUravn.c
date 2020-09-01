4026c6794e78dae3d4342017701f2ac4eea794ad
lev Bagryansky <32939651+levBagryansky@users.noreply.github.com>
2020-09-01 19:08:01 +0300

    Add files via upload

--- KvadratUravn.c
+++ KvadratUravn.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <math.h>
 
+const int infinity = -1; // if there are infinity roots
+
 int solve(double a, double b, double c, double* x1, double* x2);
 double findDiscr(double a, double b, double c);
 
 int main(){
     int nRoots;
-    double a = 0, b = 0, c = 0, x1 = 0, x2 = 0, d = 0;
+    double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
     printf("Enter coefficients a, b and c: \n");
     scanf ("%lg %lg %lg", &a, &b, &c);
     nRoots = solve(a, b, c, &x1, &x2);
@@ -19,7 +21,7 @@ int main(){
 
         case 2:  printf("x1 = %lg and x2 = %lg \n", x1, x2);
                 break;
-        case -1: printf("There is infinite roots");
+        case infinity: printf("There is infinite roots");
                 break;
 
     }
@@ -28,48 +30,49 @@ int main(){
 
 int solve(double a, double b, double c, double* x1, double* x2)
 {
+    double d = findDiscr(a, b, c);
+
     if(a == 0 and b == 0 and c == 0) // infinite
     {
-        return -1;
+        return infinity;
     }
-    else
+
+    else if(a == 0 and b == 0)     // for example 0x*x + 0x + 5 = 0
     {
-        double d = findDiscr(a, b, c);
-        if(a == 0)                          // 1 root because a == 0
-        {
-            *x1 = -c/b;
-            *x2 = *x1;
-        return 1;
-        }
-        else
-        {
-            if(d < 0)                      // 0 roots
-            {
-                return 0;
-            }
-            else
-            {
-                if(d == 0)               // 1 root because d == 0
-                {
-                    *x1 = -b / (2*a);
-                    *x2 = *x1;
-                    return 1;
-                }
-                else                         // 2 roots
-                {
-                    *x1 = (-b - sqrt(d))/2;
-                    *x2 = (-b + sqrt(d))/2;
-                    return 2;
-                }
-            }
-        }
+        return 0;
     }
+
+    else if(a == 0)                          // 1 root because a == 0
+    {
+        *x1 = -c/b;
+        *x2 = *x1;
+    return 1;
+    }
+
+    else if(d < 0)                      // 0 roots
+    {
+        return 0;
+    }
+
+    else if(d == 0)               // 1 root because d == 0
+    {
+        *x1 = -b / (2*a);
+        *x2 = *x1;
+        return 1;
+    }
+
+    else                         // 2 roots
+    {
+        *x1 = (-b - sqrt(d))/2;
+        *x2 = (-b + sqrt(d))/2;
+        return 2;
+    }
+
 }
 
 
 double findDiscr(double a, double b, double c)
 {
     double discriminant = b*b - 4*a*c;
-    printf("Discriminant of the equation: %.1f \n", discriminant);
     return discriminant;
 }
